ce1aa1cf9d0e4bbe3b69f443de74acd002f0e6f0
dchamp16 <dchamp16@gmail.com>
2022-11-15 15:55:24 -0700

    fixed index.css added linkind

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,15 +9,18 @@ const Contact = () => {
         </h1>
         <ol className="skill">
           <li>
-            <a className="fa-brands fa-github" href="https://github.com/dchamp16">
-              {" "}
-            </a>
+            <a className="fa-brands fa-github" href="https://github.com/dchamp16"></a>
             Github
           </li>
           <li>
-            <a className="fa-regular fa-envelope" href="mailto:dchamp16@gmail.com">
-              {" "}
-            </a>
+            <a
+              className="fa-brands fa-linkedin"
+              href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
+            ></a>
+            Linkedin
+          </li>
+          <li>
+            <a className="fa-regular fa-envelope" href="mailto:dchamp16@gmail.com"></a>
             Email
           </li>
         </ol>

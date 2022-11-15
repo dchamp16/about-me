49db2ed1652e8e57621887e76807bf8f676c73e5
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2022-11-14 17:19:45 -0700

    loop to nav and added style when your in active page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,39 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../assets/style/fonts/active.css";
 
-const Nav = () => {
+function Nav() {
+  let location = [
+    "/",
+    "/personal-interest",
+    "/haiku",
+    "/personal-bio",
+    "/favorite-book",
+    "skills",
+    "contact",
+  ];
+  let listName = ["About Me", "Interest", "Info", "Bio", "Books", "Skill", "Contact"];
+
+  let li = location.map((value, index) => {
+    return (
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "red" } : {};
+        }}
+        className="li"
+        key={index}
+        to={value}
+      >
+        {listName[index]}
+      </NavLink>
+    );
+  });
   return (
     <nav className="nav-bar">
-      <ul>
-        <li>
-          <NavLink to="/">About Me</NavLink>
-        </li>
-        <li>
-          <NavLink to="/personal-interest">Interest</NavLink>
-        </li>
-        <li>
-          <NavLink to="/haiku">Info</NavLink>
-        </li>
-        <li>
-          <NavLink to="/personal-bio">Bio</NavLink>
-        </li>
-        <li>
-          <NavLink to="/favorite-book">Books</NavLink>
-        </li>
-
-        {/* <li>
-            <NavLink to="schedule">Schedule</NavLink>
-          </li> */}
-        <li>
-          <NavLink to="skills">Skill</NavLink>
-        </li>
-        <li>
-          <NavLink to="contact">Contact</NavLink>
-        </li>
-      </ul>
+      <ul>{li}</ul>
     </nav>
   );
-};
+}
 
 export default Nav;
